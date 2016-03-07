904d76b152cc6aebeeb05120debd73d2c3c3777b
Manideep Polireddi <manideepiitb@gmail.com>
2016-03-07 11:30:17 +0530

    Moves the previous dotfiles to x-old

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 echo "Dependencies: vim"
 
 #Setup symlinks for vim
+mv $HOME/.vim $HOME/.vim-old
+mv $HOME/.vimrc $HOME/.vimrc-old
 ln -s $PWD/vim $HOME/.vim
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
 
@@ -10,5 +12,7 @@ ln -s $HOME/.vim/vimrc $HOME/.vimrc
 vim +PlugInstall +qall
 
 #setup symlink for bash
+mv $HOME/.bash_profile $HOME/.bash_profile-old
+mv $HOME/.zshrc $HOME/.zshrc-old
 ln -s $PWD/bash/bash_profile $HOME/.bash_profile
 ln -s $PWD/zsh/zshrc $HOME/.zshrc
