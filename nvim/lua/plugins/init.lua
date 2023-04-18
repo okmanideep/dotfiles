d6171b0970325fd278a9f848bdfa7437fe0b88a3
Manideep Polireddi <okmanideep@users.noreply.github.com>
2023-04-18 05:07:43 -0030

    Upgrade to nvim 0.9.0

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,14 +1,14 @@
 return {
 	{
 		'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
-		config = function ()
+		config = function()
 			require('Comment').setup()
 		end
 	},
 
 	{
 		'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
-		config = function ()
+		config = function()
 			require('indent_blankline').setup {
 				show_trailing_blankline_indent = false
 			}
@@ -17,22 +17,21 @@ return {
 
 	'junegunn/vim-easy-align',
 	'tpope/vim-surround', -- Add () with csaw(
-	'gpanders/editorconfig.nvim', -- Won't be required after NVIM 0.9
 	'nvim-treesitter/playground', -- Treesitter playground :TSHighlightCapturesUnderCursor
 
 	{
 		'lewis6991/gitsigns.nvim',
-		config = function ()
+		config = function()
 			require('gitsigns').setup()
 		end
 	},
 	{
 		'folke/trouble.nvim',
-		config = function ()
+		config = function()
 			require('trouble').setup()
 
-			vim.keymap.set('n', '<leader>x', '<cmd>TroubleToggle<cr>', { desc = 'LSP: Toggle Diagnostics'})
-			vim.keymap.set('n', 'gx', '<cmd>TroubleToggle document_diagnostics<cr>', { desc = 'LSP: Toggle Diagnostics'})
+			vim.keymap.set('n', '<leader>x', '<cmd>TroubleToggle<cr>', { desc = 'LSP: Toggle Diagnostics' })
+			vim.keymap.set('n', 'gx', '<cmd>TroubleToggle document_diagnostics<cr>', { desc = 'LSP: Toggle Diagnostics' })
 		end
 	},
 
