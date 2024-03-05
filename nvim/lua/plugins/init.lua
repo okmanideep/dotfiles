42a8c516480839701411964dd1dee71f11562806
Manideep Polireddi <okmanideep@users.noreply.github.com>
2024-03-05 06:36:16 -0030

    try to disable to scope highlighting in indent_blankline plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -9,7 +9,7 @@ return {
 	{
 		'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
 		main = "ibl",
-		opts = {}
+		opts = { scope = { enabled = false } },
 	},
 
 	'junegunn/vim-easy-align',
