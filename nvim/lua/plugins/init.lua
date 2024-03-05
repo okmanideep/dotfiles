a6dc8d0695fa5b27628fc9c73034958a5d5fd87c
Manideep Polireddi <okmanideep@users.noreply.github.com>
2024-03-05 05:45:26 -0030

    update plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -8,15 +8,12 @@ return {
 
 	{
 		'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
-		config = function()
-			require('indent_blankline').setup {
-				show_trailing_blankline_indent = false
-			}
-		end
+		main = "ibl",
+		opts = {}
 	},
 
 	'junegunn/vim-easy-align',
-	'tpope/vim-surround', -- Add () with csaw(
+	'tpope/vim-surround',      -- Add () with csaw(
 	'nvim-treesitter/playground', -- Treesitter playground :TSHighlightCapturesUnderCursor
 
 	{
