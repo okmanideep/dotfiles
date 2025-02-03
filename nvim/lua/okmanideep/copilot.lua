99901105b8ef6bcea379bebc05041ca710d4452a
Manideep Polireddi <okmanideep@users.noreply.github.com>
2025-02-03 11:50:30 -0030

    modify copilot shortcuts to be more home row friendly

--- nvim/lua/okmanideep/copilot.lua
+++ nvim/lua/okmanideep/copilot.lua
@@ -3,5 +3,8 @@ vim.g.copilot_filetypes = {
 	["*"] = false,
 }
 
--- explicitly request for copilot suggestions on Ctrl-Enter
-vim.keymap.set('i', '<C-CR>', '<Plug>(copilot-suggest)')
+-- explicitly request for copilot suggestions on Ctrl-H
+vim.keymap.set('i', '<C-h>', '<Plug>(copilot-suggest)', { noremap = true, desc = 'Trigger copilot suggestions' })
+
+-- accept line using Ctrl-L
+vim.keymap.set('i', '<C-l>', '<Plug>(copilot-accept-line)', { noremap = true, desc = 'Accept single line in copilot suggestion' })
