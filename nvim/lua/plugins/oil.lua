af2a71bd6018b3d23501b1bd4ca3a3744f455c84
Manideep Polireddi <okmanideep@users.noreply.github.com>
2025-02-14 18:45:08 +0530

    feat: file icon for webc

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -12,6 +12,15 @@ return {
 				show_hidden = true,
 			}
 		}
+		require('nvim-web-devicons').setup {
+			override_by_extension = {
+				["webc"] = {
+					icon = "",
+					color = "#8EB31F",
+					name = "Webc"
+				}
+			}
+		}
 
 		vim.keymap.set('n', '-', '<cmd>Oil<cr>', { desc = "Open parent directory" })
 		vim.keymap.set('n', '<leader>-', require('oil').toggle_float,
