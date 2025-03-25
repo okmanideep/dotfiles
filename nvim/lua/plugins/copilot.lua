5dcdc357d00d02a3a919a2093986b7cd997f7203
Manideep Polireddi <okmanideep@users.noreply.github.com>
2025-03-25 18:33:08 +0530

    fix: insert tab if there are no suggestions from copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -2,16 +2,35 @@ return {
 	"zbirenbaum/copilot.lua",
 	cmd = "Copilot",
 	event = "InsertEnter",
-	config = function ()
+	config = function()
 		require("copilot").setup({
 			suggestion = {
 				auto_trigger = false,
 				keymap = {
-					accept = "<Tab>",
+					accept = false,
 					next = "<C-h>",
 					accept_line = "<C-l>",
 				}
 			}
 		})
+
+		local suggestion = require('copilot.suggestion')
+		vim.keymap.set('i', '<Tab>', function()
+			if (suggestion.is_visible()) then
+				suggestion.accept()
+			else
+				local expandtab = vim.bo.expandtab
+				local shiftwidth = vim.bo.shiftwidth
+				local indent_str
+
+				if expandtab then
+					indent_str = string.rep(" ", shiftwidth)
+				else
+					indent_str = "\t"
+				end
+
+				vim.api.nvim_put({ indent_str }, 'c', false, true)
+			end
+		end, { noremap = true, expr = false })
 	end,
 }
