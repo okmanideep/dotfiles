ca5cf7fe9189d1d99f3aef2c34cf5c2d43e65a50
Manideep Polireddi <okmanideep@users.noreply.github.com>
2025-04-09 18:59:17 +0530

    nvim: disable diagrams because of an issue in nvim 0.11

--- nvim/lua/okmanideep/diagrams.lua
+++ nvim/lua/okmanideep/diagrams.lua
@@ -1,29 +1,32 @@
-local Hydra = require'hydra'
-local hint = [[
- Arrow^^^^^^   Select region with <C-v> 
- ^ ^ _K_ ^ ^   _f_: surround it with box
- _H_ ^ ^ _L_
- ^ ^ _J_ ^ ^                      _<Esc>_
-]]
-
-Hydra({
-   name = 'Draw Diagram',
-   hint = hint,
-   config = {
-      color = 'pink',
-      invoke_on_body = true,
-      on_enter = function()
-         vim.o.virtualedit = 'all'
-      end,
-   },
-   mode = 'n',
-   body = 'dg',
-   heads = {
-      { 'H', '<C-v>h:VBox<CR>' },
-      { 'J', '<C-v>j:VBox<CR>' },
-      { 'K', '<C-v>k:VBox<CR>' },
-      { 'L', '<C-v>l:VBox<CR>' },
-      { 'f', ':VBox<CR>', { mode = 'v' }},
-      { '<Esc>', nil, { exit = true } },
-   }
-})
+-- Due to https://github.com/anuvyklack/hydra.nvim/issues/108
+-- This is failing in nvim 0.11
+--
+-- local Hydra = require'hydra'
+-- local hint = [[
+--  Arrow^^^^^^   Select region with <C-v> 
+--  ^ ^ _K_ ^ ^   _f_: surround it with box
+--  _H_ ^ ^ _L_
+--  ^ ^ _J_ ^ ^                      _<Esc>_
+-- ]]
+--
+-- Hydra({
+--    name = 'Draw Diagram',
+--    hint = hint,
+--    config = {
+--       color = 'pink',
+--       invoke_on_body = true,
+--       on_enter = function()
+--          vim.o.virtualedit = 'all'
+--       end,
+--    },
+--    mode = 'n',
+--    body = '<leader>dg',
+--    heads = {
+--       { 'H', '<C-v>h:VBox<CR>' },
+--       { 'J', '<C-v>j:VBox<CR>' },
+--       { 'K', '<C-v>k:VBox<CR>' },
+--       { 'L', '<C-v>l:VBox<CR>' },
+--       { 'f', ':VBox<CR>', { mode = 'v' }},
+--       { '<Esc>', nil, { exit = true } },
+--    }
+-- })
