14f4b67f4f822719528d650299fdfa1bb4c27c88
Manideep Polireddi <okmanideep@users.noreply.github.com>
2025-06-06 16:15:52 +0530

    nvim: udpate keymap for floating directory viewer

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -23,7 +23,7 @@ return {
 		}
 
 		vim.keymap.set('n', '-', '<cmd>Oil<cr>', { desc = "Open parent directory" })
-		vim.keymap.set('n', '<leader>-', require('oil').toggle_float,
+		vim.keymap.set('n', '<leader>d', require('oil').toggle_float,
 			{ desc = "Open parent deirectory in a floating window" })
 		vim.keymap.set('n', 'gm', function() require('oil').set_sort({ { "mtime", "desc" } }) end,
 			{ desc = "Sort by modified time in descending order" })
