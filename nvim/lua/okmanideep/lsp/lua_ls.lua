fec788c44104f9a7c90e0f10585b34741833aed0
Manideep Polireddi <5300499+okmanideep@users.noreply.github.com>
2025-11-29 13:28:41 +0530

    nvim: load nvim's lua API in lua_ls for documentation help while editing dotfiles

--- nvim/lua/okmanideep/lsp/lua_ls.lua
+++ nvim/lua/okmanideep/lsp/lua_ls.lua
@@ -4,7 +4,7 @@ return {
 			runtime = {
 				version = "Lua 5.1", -- Neovim embeds LuaJIT (Lua 5.1)
 			},
-			workspace = { checkThirdParty = false },
+			workspace = { checkThirdParty = false, library = vim.api.nvim_get_runtime_file('', true) },
 			telemetry = { enable = false },
 			diagnostics = { globals = { 'vim' } },
 		},
