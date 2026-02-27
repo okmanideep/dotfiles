457003bbd7f4d4adea4c87569724c8814dfe1993
Manideep Polireddi <okmanideep@users.noreply.github.com>
2026-02-27 13:05:37 +0530

    nvim: markdown checkbox toggle

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,3 +1,18 @@
 -- Disable diagnostics by default for markdown files
 -- Too much spell check noise
 vim.diagnostic.enable(false, { bufnr = 0 })
+
+vim.keymap.set("n", "gt", function()
+  local current_line = vim.api.nvim_get_current_line()
+  local updated_line
+
+  if current_line:match("%- %[%s%]") then
+    updated_line = current_line:gsub("%- %[%s%]", "- [x]", 1)
+  elseif current_line:match("%- %[[xX]%]") then
+    updated_line = current_line:gsub("%- %[[xX]%]", "- [ ]", 1)
+  else
+    return
+  end
+
+  vim.api.nvim_set_current_line(updated_line)
+end, { buffer = true, desc = "Toggle markdown checkbox" })
