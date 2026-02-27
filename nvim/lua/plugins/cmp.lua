fcf4e9ee3da7a22d99298dae4e5910f278523e69
Manideep Polireddi <okmanideep@users.noreply.github.com>
2026-02-27 13:05:24 +0530

    nvim: emoji completion

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -2,13 +2,13 @@
 return {
 	{
 		'saghen/blink.cmp',
-		dependencies = 'L3MON4D3/LuaSnip',
+		dependencies = {
+			'L3MON4D3/LuaSnip',
+			'moyiz/blink-emoji.nvim',
+		},
 		version = '1.*',
 		event = { "InsertEnter", "CmdlineEnter" },
 		opts = {
-			enabled = function ()
-				return vim.bo.filetype ~= "markdown"
-			end,
 			keymap = {
 				['<CR>'] = { 'accept', 'fallback' },
 				['<C-\\>'] = { 'hide', 'fallback' },
@@ -32,6 +32,48 @@ return {
 			},
 			sources = {
 				default = { 'lsp', 'path', 'snippets', 'buffer' },
+				per_filetype = {
+					markdown = { 'path', 'emoji' },
+				},
+				providers = {
+					emoji = {
+						module = 'blink-emoji',
+						name = 'Emoji',
+						opts = {
+							insert = true,
+							trigger = function()
+								return { ':' }
+							end,
+						},
+						transform_items = function(_, items)
+							local line = vim.api.nvim_get_current_line()
+							local cursor = vim.api.nvim_win_get_cursor(0)
+							local before = line:sub(1, cursor[2] + 1)
+							local keyword = before:match(':[%w_+%-]*$')
+							if not keyword or #keyword <= 1 then
+								return items
+							end
+
+							local aliases = {
+								tada = '🎉',
+							}
+
+							local alias_items = {}
+							for name, emoji in pairs(aliases) do
+								local alias_label = ':' .. name .. ':'
+								if alias_label:sub(1, #keyword) == keyword then
+									alias_items[#alias_items + 1] = {
+										label = alias_label,
+										filterText = alias_label,
+										insertText = emoji,
+									}
+								end
+							end
+
+							return vim.list_extend(alias_items, items)
+						end,
+					},
+				},
 			},
 		},
 		config = function(_, opts)
